67ada2705336624746cc53f4c6aae363b90d533f
Alexander Dovgalenko <a.dovgalenko2@gmail.com>
2022-06-19 00:54:57 +0300

    more code clean ups )) (#3)

--- general_emoji.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class GeneralEmojies(Enum):
-    EXCLAMATION_MARK_RED = "\U00002757"
-    CHECK_MARK = "\U00002705"
-    BULLET_MARK = "➣"
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import logging
 
-from aiogram import executor, types
+from aiogram import executor
 
-# from telegram import Update, ParseMode, KeyboardButton, ReplyKeyboardMarkup
-# from telegram.ext import CommandHandler, Updater, CallbackQueryHandler, CallbackContext, MessageHandler
 from bot_init import dp
-from keyboards.common_emoji_codes import options_emoji, weather_forecast_emoji
 from handlers import user_options_handlers, commands_handlers, general_handlers
 
 test_data = []
@@ -23,36 +20,10 @@ logger = logging.getLogger(__name__)
 async def on_startup(_):
     logger.info("Bot went Online!!!")
 
-# def reply_current_weather_result(update: Update, context: CallbackContext):
-#     chat_id = update.message.chat_id
-#     message_id = update.message.message_id
-#     weather_text = compile_current_weather_output(city_name=update.message.text, weather_provider=WEATHER_PROVIDER)
-#     # keyboard = [[KeyboardButton(text="Оберіть назву населеного пункту...")]]
-#     # reply_markup = ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True, one_time_keyboard=True,)
-#     # reply_markup = InlineKeyboardMarkup(keyboard)=
-#     context.bot.send_message(text=weather_text, chat_id=chat_id, parse_mode=ParseMode.HTML)
-
-
-# def reply_start_keyboard():
-#     start_buttons = [options_emoji, weather_forecast_emoji]
-#     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-#     keyboard.add(*start_buttons)
-#     return keyboard
-
 
 commands_handlers.register_commands_handlers(dp)
 user_options_handlers.register_user_options_handlers(dp)
 general_handlers.register_general_handlers(dp)
-
-
-# @dp.message_handler(Text(equals="\U0001F6E0"))
-# async def handle_user_options(  )
-
-
-# def user_options(update: Update, _: CallbackContext) -> None:
-#     chat_id = update.message.chat_id
-#     reply_markup = user_options_keyboard()
-#     update.message.bot.send_message(text='Select a language', reply_markup=reply_markup, chat_id=chat_id)
 
 
 def launch_bot():
